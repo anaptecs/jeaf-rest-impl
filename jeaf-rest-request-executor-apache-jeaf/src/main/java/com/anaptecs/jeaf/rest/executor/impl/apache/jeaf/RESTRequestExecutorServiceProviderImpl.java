c1fd6011cfa0b95459035885397db084ac88d92e
JEAF Development Team <tls@anaptecs.de>
2022-09-30 08:31:46 +0200

    Added JEAF Service Provider impl as request executor

--- jeaf-rest-request-executor-apache-jeaf/src/main/java/com/anaptecs/jeaf/rest/executor/impl/apache/jeaf/RESTRequestExecutorServiceProviderImpl.java
+++ jeaf-rest-request-executor-apache-jeaf/src/main/java/com/anaptecs/jeaf/rest/executor/impl/apache/jeaf/RESTRequestExecutorServiceProviderImpl.java
@@ -3,7 +3,7 @@
  *
  * All rights reserved.
  */
-package com.anaptecs.jeaf.rest.executor.impl.apache.spring;
+package com.anaptecs.jeaf.rest.executor.impl.apache.jeaf;
 
 import java.io.IOException;
 import java.net.URI;
@@ -13,21 +13,23 @@ import java.util.Map;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
 import org.apache.hc.core5.http.ContentType;
 import org.apache.hc.core5.http.HttpEntity;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
-import org.zalando.problem.Problem;
-import org.zalando.problem.ProblemBuilder;
-import org.zalando.problem.Status;
-import org.zalando.problem.ThrowableProblem;
-import org.zalando.problem.spring.web.advice.HttpStatusAdapter;
 
+import com.anaptecs.jeaf.core.spi.ServiceProviderImplementation;
+import com.anaptecs.jeaf.json.api.JSON;
+import com.anaptecs.jeaf.json.problem.Problem;
+import com.anaptecs.jeaf.json.problem.Problem.Builder;
+import com.anaptecs.jeaf.json.problem.RESTProblemException;
 import com.anaptecs.jeaf.rest.executor.api.RESTRequestExecutor;
 import com.anaptecs.jeaf.rest.executor.impl.apache.AbstractApacheHttpClientRESTRequestExecutorBase;
 import com.anaptecs.jeaf.rest.executor.impl.config.RESTClientConfiguration;
 import com.anaptecs.jeaf.rest.executor.impl.config.yaml.YAMLBasedRESTClientConfigurationLoader;
+import com.anaptecs.jeaf.tools.api.http.HTTPStatusCode;
+import com.anaptecs.jeaf.xfun.api.XFun;
+import com.anaptecs.jeaf.xfun.api.common.ComponentID;
+import com.anaptecs.jeaf.xfun.api.errorhandling.SystemException;
+import com.anaptecs.jeaf.xfun.api.health.CheckLevel;
+import com.anaptecs.jeaf.xfun.api.health.HealthCheckResult;
+import com.anaptecs.jeaf.xfun.api.trace.Trace;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
@@ -35,56 +37,72 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  * Configurations are read from YAML files via the classpath. The configuration file must have the lower case simple
  * name of the service.
  * 
- * Zalando Problem library is used for JSON Problem handling.
- * 
  * @author JEAF Development Team
  */
-@Component
-public class ApacheSpringHttpClientRESTRequestExecutor extends AbstractApacheHttpClientRESTRequestExecutorBase {
+public class RESTRequestExecutorServiceProviderImpl extends AbstractApacheHttpClientRESTRequestExecutorBase implements
+    ServiceProviderImplementation {
+
   /**
-   * Logger for this class.
+   * Reference to the object that identifies this component. The reference is never null.
    */
-  private static final Logger LOGGER = LoggerFactory.getLogger(ApacheSpringHttpClientRESTRequestExecutor.class);
+  private static final ComponentID COMPONENT_ID;
+
+  private static final Trace TRACE;
+
+  /**
+   * Static initializer is used to create the components ComponentID object and its trace object.
+   */
+  static {
+    // Create Component ID and trace object.
+    Package lBasePackage = RESTRequestExecutorServiceProviderImpl.class.getPackage();
+    COMPONENT_ID = new ComponentID("RESTRequestExecutorServiceProviderImpl", lBasePackage.getName());
+
+    TRACE = XFun.getTraceProvider().getTrace(COMPONENT_ID);
+  }
 
   /**
    * Map contains all loaded configurations. Configurations will only be loaded on demand.
    */
   private Map<Class<?>, RESTClientConfiguration> configurations = new HashMap<>();
 
-  /**
-   * Object mapper is used for serialization and deserialization of objects from Java to JSON and vice versa.
-   */
-  @Autowired
-  private ObjectMapper objectMapper;
+  @Override
+  public HealthCheckResult check( CheckLevel pLevel ) {
+    return null;
+  }
+
+  @Override
+  public void initialize( ) throws SystemException {
+    // Nothing to do.
+  }
 
   @Override
   protected ObjectMapper getObjectMapper( ) {
-    return objectMapper;
+    return JSON.getJSONTools().getDefaultObjectMapper();
   }
 
   @Override
   protected boolean isRequestTracingEnabled( RESTClientConfiguration pConfiguration ) {
-    return pConfiguration.traceRequests() && LOGGER.isInfoEnabled();
+    return pConfiguration.traceRequests() && TRACE.isInfoEnabled();
   }
 
   @Override
   protected boolean isResponseTracingEnabled( RESTClientConfiguration pConfiguration ) {
-    return pConfiguration.traceResponses() && LOGGER.isInfoEnabled();
+    return pConfiguration.traceResponses() && TRACE.isInfoEnabled();
   }
 
   @Override
   protected void traceRequest( String pRequestLog ) {
-    LOGGER.info(pRequestLog);
+    TRACE.info(pRequestLog);
   }
 
   @Override
   protected void traceResponse( String pResponseLog ) {
-    LOGGER.info(pResponseLog);
+    TRACE.info(pResponseLog);
   }
 
   @Override
   protected void traceException( String pErrorMessage, Exception pException ) {
-    LOGGER.error(pErrorMessage, pException);
+    TRACE.error(pErrorMessage, pException);
   }
 
   @Override
@@ -100,7 +118,6 @@ public class ApacheSpringHttpClientRESTRequestExecutor extends AbstractApacheHtt
 
   @Override
   protected RuntimeException processErrorResponse( URI pRequestURI, CloseableHttpResponse pResponse ) {
-
     // Try to read error response from body
     String lResponseBody;
     HttpEntity lEntity = pResponse.getEntity();
@@ -118,10 +135,10 @@ public class ApacheSpringHttpClientRESTRequestExecutor extends AbstractApacheHtt
     }
 
     // REST resource returned problem JSON.
-    ThrowableProblem lProblem;
+    Problem lProblem;
     if (lResponseBody != null && ContentType.APPLICATION_PROBLEM_JSON.getMimeType().equals(lEntity.getContentType())) {
       try {
-        lProblem = this.getObjectMapper().readValue(lResponseBody, ThrowableProblem.class);
+        lProblem = this.getObjectMapper().readValue(lResponseBody, Problem.class);
       }
       catch (IOException e) {
         throw this.processInternalServerError(pRequestURI, e, "Unable to process problem JSON");
@@ -134,30 +151,30 @@ public class ApacheSpringHttpClientRESTRequestExecutor extends AbstractApacheHtt
     // For whatever reason we were not yet able to build a problem. So let's try to build it up from what we have.
     if (lProblem == null) {
       // Try to resolve some details.
-      ProblemBuilder lProblemBuilder = Problem.builder();
-      HttpStatusAdapter lStatus = new HttpStatusAdapter(HttpStatus.valueOf(pResponse.getCode()));
-      lProblemBuilder.withStatus(lStatus);
-      lProblemBuilder.withTitle(lStatus.getReasonPhrase());
-      lProblemBuilder.withType(pRequestURI);
-      lProblemBuilder.withDetail(lResponseBody);
+      Builder lProblemBuilder = Problem.builder();
+      HTTPStatusCode lStatusCode = HTTPStatusCode.fromStatusCode(pResponse.getCode());
+      lProblemBuilder.setStatus(lStatusCode.getCode());
+      lProblemBuilder.setTitle(lStatusCode.getName());
+      lProblemBuilder.setType(pRequestURI.toString());
+      lProblemBuilder.setDetail(lResponseBody);
       lProblem = lProblemBuilder.build();
     }
 
     // Finally throw Problem
-    throw lProblem;
+    throw new RESTProblemException(lProblem);
   }
 
   @Override
   protected RuntimeException processInternalServerError( URI pRequestURI, Exception pException,
       String pContext ) {
-    LOGGER.error(pContext, pException);
-    ProblemBuilder lProblemBuilder = Problem.builder();
-    Status lStatus = Status.INTERNAL_SERVER_ERROR;
-    lProblemBuilder.withStatus(lStatus);
-    lProblemBuilder.withTitle(lStatus.getReasonPhrase());
-    lProblemBuilder.withType(pRequestURI);
-    lProblemBuilder.withDetail(pException.getMessage());
-    throw lProblemBuilder.build();
+    TRACE.error(pContext, pException);
+    Builder lProblemBuilder = Problem.builder();
+    HTTPStatusCode lStatusCode = HTTPStatusCode.INTERNAL_SERVER_ERROR;
+    lProblemBuilder.setStatus(lStatusCode.getCode());
+    lProblemBuilder.setTitle(lStatusCode.getName());
+    lProblemBuilder.setType(pRequestURI.toString());
+    lProblemBuilder.setDetail(pException.getMessage());
+    return new RESTProblemException(lProblemBuilder.build());
   }
 
 }
