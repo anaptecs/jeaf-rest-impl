60cda8929b9175ece7488ff01e480c0d12dfd7b2
JEAF Development Team <tls@anaptecs.de>
2023-06-15 06:42:52 +0200

    Updated parent POM and prepared release.

--- jeaf-rest-request-executor-apache-spring/src/main/java/com/anaptecs/jeaf/rest/executor/impl/apache/spring/YAMLBasedRESTClientConfigurationLoader.java
+++ jeaf-rest-request-executor-apache-spring/src/main/java/com/anaptecs/jeaf/rest/executor/impl/apache/spring/YAMLBasedRESTClientConfigurationLoader.java
@@ -8,9 +8,12 @@ package com.anaptecs.jeaf.rest.executor.impl.apache.spring;
 import java.io.IOException;
 import java.io.InputStream;
 
+import org.yaml.snakeyaml.LoaderOptions;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
+import org.yaml.snakeyaml.inspector.TagInspector;
 import org.yaml.snakeyaml.introspector.BeanAccess;
+import org.yaml.snakeyaml.nodes.Tag;
 
 import com.anaptecs.jeaf.rest.executor.impl.config.RESTClientConfiguration;
 import com.anaptecs.jeaf.rest.executor.impl.config.RESTClientConfigurationImpl;
@@ -29,7 +32,7 @@ public class YAMLBasedRESTClientConfigurationLoader {
   }
 
   public static RESTClientConfiguration loadConfiguration( String pResource ) {
-    Constructor lConstructor = new Constructor(RESTClientConfigurationImpl.class);
+    Constructor lConstructor = new Constructor(RESTClientConfigurationImpl.class, getSecureSnakeYAMLOptions());
     Yaml lYAML = new Yaml(lConstructor);
     lYAML.setBeanAccess(BeanAccess.FIELD);
     ClassLoader lClassLoader = YAMLBasedRESTClientConfigurationLoader.class.getClassLoader();
@@ -48,5 +51,16 @@ public class YAMLBasedRESTClientConfigurationLoader {
       throw new IllegalArgumentException("Unable to load YAML configuration '" + pResource
           + "'. " + e.getMessage());
     }
+  }
+
+  private static LoaderOptions getSecureSnakeYAMLOptions( ) {
+    LoaderOptions lLoaderOptions = new LoaderOptions();
+    lLoaderOptions.setTagInspector(new TagInspector() {
+      @Override
+      public boolean isGlobalTagAllowed( Tag pTag ) {
+        return false;
+      }
+    });
+    return lLoaderOptions;
   }
 }
